48d53a5e27b7168e15f93424476dfbccace1684d
Oskar Köök <oskarkook@maatriks.ee>
2020-01-23 15:01:54 +0200

    add Jason and compile test helpers

--- mix.exs
+++ mix.exs
@@ -16,6 +16,7 @@ defmodule ELBCognitoPlug.MixProject do
       },
       version: "0.1.0",
       elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -28,12 +29,17 @@ defmodule ELBCognitoPlug.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:plug, "~> 1.8.3"},
       {:joken, "~> 2.2"},
-      {:tesla, "~> 1.2.1"}
+      {:tesla, "~> 1.2.1"},
+      {:jason, "~> 1.1"}
     ]
   end
 end
